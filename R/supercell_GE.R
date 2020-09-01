7dfdb6df4aa1aa151d2d3d2a1e5502f2d3593471
mariiabilous <mariia.bilous@unil.ch>
2020-09-01 14:57:18 +0200

    added weighted averaging (for averagind gene expression of super-cells)

--- R/supercell_GE.R
+++ R/supercell_GE.R
@@ -5,22 +5,36 @@
 #'
 #' @param ge gene expression matrix (or any coordinate matrix) with genes as rows and cells as cols
 #' @param groups vector of membership (assignment of single-cell to super-cells)
+#' @param weights vector of a cell weight (NULL by default), used for computing average gene expression withing cluster of super-cells
 #' @param do.median.norm whether to normalize
 #'
 #' @return a matrix of simplified (averaged withing groups) data with ncol equal to number of groups and nrows as in the initial dataset
 #' @export
 
-supercell_GE  <- function(ge, groups, do.median.norm = FALSE){
+supercell_GE  <- function(ge, groups, weights = NULL, do.median.norm = FALSE){
+
+  if(ncol(ge) != length(groups)){
+    stop("Length of the vector groups has to be equal to the number of cols in matrix ge")
+  }
+
   goups.idx <-  plyr:::split_indices(groups)
 
-  fun <- function(idx){
-    Matrix::rowMeans(ge[, idx, drop = FALSE])
+  if(is.null(weights)){
+    fun <- function(idx){
+      Matrix::rowMeans(ge[, idx, drop = FALSE])
+    }
+  } else {
+    if(length(weights) != length(groups))
+      stop("weights must be the same length as groups or NULL in case of unweighted averaging")
+    fun <- function(idx){
+      matrixStats::rowWeightedMeans(ge[, idx, drop = FALSE], w = weights[idx])
+    }
   }
-  supercell.GE             <- sapply(goups.idx, fun)
 
-  if(!(TRUE %in% is.na(as.numeric(colnames(supercell.GE))))){
-    supercell.GE <- supercell.GE[,order(as.numeric(colnames(supercell.GE)))]
-  }
+  supercell.GE             <- sapply(goups.idx, fun)
+  # if(!(TRUE %in% is.na(as.numeric(colnames(supercell.GE))))){
+  #   supercell.GE <- supercell.GE[,order(as.numeric(colnames(supercell.GE)))]
+  # }
 
   if(do.median.norm){
     supercell.GE <- (supercell.GE+0.01)/apply(supercell.GE+0.01, 1, median)
