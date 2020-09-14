689e966d10b3c05ed6e8b082599f614b5934205b
mariiabilous <mariia.bilous@unil.ch>
2020-09-14 13:29:37 +0200

    option of log scaling of super-cell size and rescaling of edge weight (level of attration)

--- R/supercell_plot.R
+++ R/supercell_plot.R
@@ -14,10 +14,35 @@
 #' @export
 
 
-supercell_plot <- function(SC.nw, group = NULL, color.use = NULL, lay.method = c("nicely", "fr", "components"), lay = NULL, seed = 12345, main = NA, do.frames = TRUE){
+supercell_plot <- function(SC.nw,
+                           group = NULL,
+                           color.use = NULL,
+                           lay.method = c("nicely", "fr", "components"),
+                           lay = NULL,
+                           seed = 12345,
+                           main = NA,
+                           do.frames = TRUE,
+                           do.extra.log.rescale = FALSE,
+                           log.base = 2,
+                           do.extra.sqtr.rescale = FALSE,
+                           frame.color = "black",
+                           weights = NULL,
+                           min.cell.size = 0){
+
+
+
   N.SC <- igraph::vcount(SC.nw)
   if(is.null(igraph::V(SC.nw)$size)) igraph::V(SC.nw)$size <- 1
   vsize <- sqrt(igraph::V(SC.nw)$size)
+
+  cells.to.remove <- which(vsize < min.cell.size)
+  cells.to.use    <- setdiff(1:N.SC, cells.to.remove)
+  print(cells.to.remove)
+
+  if(do.extra.log.rescale)
+    vsize <- log(vsize+1, base = log.base)
+  if(do.extra.sqtr.rescale)
+    vsize <- sqrt(vsize)
 
   if(is.null(group)) group <- rep(1, N.SC)
   if(length(group) != N.SC) stop(paste("Vector groups has to be the same length as number of super-cells:", N.SC))
@@ -31,6 +56,21 @@ supercell_plot <- function(SC.nw, group = NULL, color.use = NULL, lay.method = c
     names(color.use) <- sort(unique(group))
   }
 
+  if(!is.null(weights) & length(cells.to.remove)>0){
+    weights <- weights[-sort(unique(unlist(igraph::incident_edges(SC.nw, cells.to.remove))))]
+    print("length(weights)")
+    print(length(weights))
+
+  }
+
+  if(length(cells.to.remove)>0){
+    SC.nw <- igraph::delete_vertices(SC.nw, cells.to.remove)
+    print("igraph::ecount(SC.nw)")
+    print(igraph::ecount(SC.nw))
+  }
+
+
+
 
   if(is.null(lay)){ # compute new layout
     lay.method <- lay.method[1]
@@ -42,9 +82,9 @@ supercell_plot <- function(SC.nw, group = NULL, color.use = NULL, lay.method = c
 
     set.seed(seed)
     if(lay.method == 1)
-      lay <- igraph::layout_nicely(SC.nw)
+      lay <- igraph::layout_nicely(SC.nw, weights = weights)
     else if (lay.method == 2)
-      lay <- igraph::layout_with_fr(SC.nw)
+      lay <- igraph::layout_with_fr(SC.nw, weights = weights)
     else if (lay.method == 3)
       lay <- igraph::layout_components(SC.nw)
     else stop(paste("Unknown lay.method", lay.method))
@@ -52,9 +92,13 @@ supercell_plot <- function(SC.nw, group = NULL, color.use = NULL, lay.method = c
 
 
   v.colors <- color.use[group]
-  v.frame.colors <- ifelse(do.frames, "black", v.colors)
+  ifelse(do.frames, v.frame.colors <- rep(frame.color, length(v.colors)), v.frame.colors <- v.colors)
 
-  plot(SC.nw, vertex.label = NA, vertex.color = v.colors, vertex.frame.color = v.colors,
-            layout = lay, main = main, vertex.size = vsize)
+  plot(SC.nw, vertex.label = NA, vertex.color = v.colors[cells.to.use],
+       vertex.frame.color = v.frame.colors[cells.to.use],
+       layout = lay, main = main, vertex.size = vsize[cells.to.use])
+
+  p <- grDevices::recordPlot()
+  return(p)
 
 }
