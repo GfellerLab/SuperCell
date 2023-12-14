df3a033c1b835128f11dcf88ee968edc368dfd5a
leonardHerault <leo.herault9338@gmail.com>
2023-12-14 15:12:02 +0100

    adding seurat_v5 compatibility

--- R/supercell_2_Seurat.R
+++ R/supercell_2_Seurat.R
@@ -173,7 +173,7 @@ supercell_2_Seurat <- function(SC.GE, SC, fields = c(),
     warning("Super-cell graph was not found in SC object, no super-cell graph was added to Seurat object")
   }
   
-  if(packageVersion("Seurat") >= 5 & output.assay.version = "v5"){
+  if(packageVersion("Seurat") >= 5 & output.assay.version == "v5"){
     m.seurat[["RNA"]] <- as(object = m.seurat[["RNA"]], Class = "Assay5")
   }
   return(m.seurat)
